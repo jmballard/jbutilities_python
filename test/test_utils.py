6b09257f26ad2c8d7a5bc5b5fa25fb94c2204b1a
Julie Ballard <bronnimannj@gmail.com>
2025-07-24 15:20:31 +0100

    add tests for util files

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,9 +1,16 @@
 import os
 import unittest
+from datetime import datetime
 from pathlib import Path
+
+import pandas as pd
+
+from toolkit.utils import misc
+from toolkit.utils.get_variable_name import get_local_variable_name
 
 # functions to test
 from toolkit.utils.iohandler import IOHandler
+from toolkit.utils.snake_case import snake_case
 
 
 class TestUtils(unittest.TestCase):
@@ -37,6 +44,41 @@ class TestUtils(unittest.TestCase):
         # test the serial methods
         assert io_actual.find_last_version("test_file_path") is None
         assert io_actual.next_version("test_file_path") == io_expected["test_file_path"]
+
+    def test_get_variable_name(self):
+        a = 3
+
+        def foo(x):
+            return get_local_variable_name(x)
+
+        assert foo(a) == "a"
+        assert get_local_variable_name(a) is None
+
+    def test_misc(self):
+        input_dict = {"a": [1, 2], "b": ["A", "B"]}
+
+        assert all(
+            misc.expand_grid(input_dict)
+            == pd.DataFrame(
+                {
+                    "a": [1, 1, 2, 2],
+                    "b": [
+                        "A",
+                        "B",
+                        "A",
+                        "B",
+                    ],
+                }
+            )
+        )
+
+        mystr = f"({round(1, 3):.{3}f}) - ["
+        mystr += datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        mystr += "]"
+        assert misc.step_time(step=1, digits=3)
+
+    def test_snake_case(self):
+        assert "aasdkjh213987-a-kh" == snake_case(string="aasdkjh213987_AKh")
 
 
 if __name__ == "__main__":
